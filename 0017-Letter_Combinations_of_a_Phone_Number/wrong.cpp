ae5ed51498e38f19760b30758c143519ef5a0e23
oliver-zeng <964994927@qq.com>
2020-01-24 20:21:08 +0800

    0017

--- 0017-Letter_Combinations_of_a_Phone_Number/wrong.cpp
+++ 0017-Letter_Combinations_of_a_Phone_Number/wrong.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+using namespace std;
+
 class Solution {
 private:
     // 数组元素赋初值，用逗号隔开，最后一个元素没有逗号
@@ -19,17 +25,15 @@ private:
     int max_index;
 
     // tmp里记录的是[0, index-1]的字符串，现在要讨论的是digits[index]翻译出的字符
-    // 1
     void represent(string &digits, int index, const string &tmp) {
-        char c = digits[index];
-        //int num = transfer[digits[index] - '0'].size();
-        assert (c >= '0' && c<='9');
-        int num = transfer[c - '0'].size();
-
         if (index == max_index) {
             res.push_back(tmp);
             return;
         }
+
+        char c = digits[index];
+        int num = transfer[c - '0'].size();
+
         for (int i = 0; i < num; i++) {
             represent (digits, index + 1, tmp + transfer[digits[index] - '0'][i]);
         }
@@ -44,4 +48,12 @@ public:
     }
 };
 
+// 测试用例
+void printVec(const vector<string>& vec){
+    for(string s: vec)
+        cout << s << endl;
+}
 
+int main() {
+    printVec(Solution().letterCombinations("23"));
+}
