82a6b3ac09445cc1ec117bc7eba90ce363bd297d
oliver-zeng <964994927@qq.com>
2020-01-24 23:18:21 +0800

    0017 add debug log

--- 0017-Letter_Combinations_of_a_Phone_Number/main.cpp
+++ 0017-Letter_Combinations_of_a_Phone_Number/main.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 class Solution {
 private:
+    bool g_debug; // = true;
+
     // 数组元素赋初值，用逗号隔开，最后一个元素没有逗号
     // {}用于数组元素，或结构体赋初值
     string transfer[10] = {
@@ -26,15 +28,28 @@ private:
 
     // tmp里记录的是[0, index-1]的字符串，现在要讨论的是digits[index]翻译出的字符
     void represent(string &digits, int index, const string &tmp) {
+        // 在递归调用一开始把每次递归调用的参数打出来
+        if (g_debug)
+            cout << index << " : " << tmp << endl;
         if (index == max_index) {
             res.push_back(tmp);
+            // 在递归调用结束准备返回前，把递归树到叶子后得到的返回结果打出来
+            if (g_debug)
+                cout << "get " << tmp << " return" << endl;
             return;
         }
         int num = transfer[digits[index] - '0'].size();
 
+        // 分别在分叉前后打log，展示回溯过程
         for (int i = 0; i < num; i++) {
+            // 在递归自身调用前，把递归选择的分支打出来
+            if (g_debug)
+                cout << "digits[" << index << "] = " << digits[index] << " use " << transfer[digits[index] - '0'][i] << endl;
             represent (digits, index + 1, tmp + transfer[digits[index] - '0'][i]);
         }
+        // 在递归调用结束准备回溯时，把要回溯的递归树节点位置打出来
+        if (g_debug)
+            cout << "digits[" << index << "] = " << digits[index] << " done, recall " << endl;
         return;
     }
 
