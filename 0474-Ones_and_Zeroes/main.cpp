6bde187bf72876f9299fe953053d82bb2cfd51ab
oliver-zeng <964994927@qq.com>
2020-01-26 22:34:40 +0800

    0474 done

--- 0474-Ones_and_Zeroes/main.cpp
+++ 0474-Ones_and_Zeroes/main.cpp
@@ -1,24 +1,23 @@
 class Solution {
 private:
     int sz;
-    vector<int> memo;
+    vector<vector<vector<int>>> memo;
     vector<int> cost_0;
     vector<int> cost_1;
+    // calcMax是在m个0，n个1的消耗下选完第index个字串后的最值
     int calcMax(vector<string>& strs, int index, int m, int n) {
-        if (index >= sz) return 0;
-        int res = 0;
-        for (int i = index; i < sz; i++) {
-            res = ((cost_0[index] < m) && (cost_1[index] < n)) ?
-                max(1 + calcMax(strs, index + 1, m - cost_0[index], n - cost_1[index]), calcMax(strs, index + 1, m, n)) : calcMax(strs, index + 1, m, n);
-        }
-        return res;
+        if (index < 0) return 0;
+        if (memo[index][m][n] != -1) return memo[index][m][n];
+        memo[index][m][n] = ((cost_0[index] <= m) && (cost_1[index] <= n)) ?
+            max(1 + calcMax(strs, index - 1, m - cost_0[index], n - cost_1[index]), calcMax(strs, index - 1, m, n)) : calcMax(strs, index - 1, m, n);
+        return  memo[index][m][n];
     }
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
         // 背包组合问题，在不超过m个0，n个1的情况下，在strs中选最多item
         // 考虑item i选，与不选，对结果与消耗的影响
         sz = strs.size();
-        memo = vector<int>(sz, -1);
+        memo = vector<vector<vector<int>>>(sz, vector<vector<int>>(m + 1, vector<int>(n + 1, -1)));
         cost_0 = vector<int>(sz, 0);
         cost_1 = vector<int>(sz, 0);
         for (int i = 0; i < sz; i++) {
@@ -29,6 +28,7 @@ public:
                     cost_1[i]++;
             }
         }
-        return calcMax(strs, 0, m, n);
+        return calcMax(strs, sz - 1, m, n);
     }
 };
+
