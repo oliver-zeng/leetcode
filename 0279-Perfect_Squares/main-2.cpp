c1924aa5a23eaeacfc5e4aaeffc38c7895ec8c7c
oliver-zeng <964994927@qq.com>
2020-02-19 21:14:40 +0800

    update 0279

--- 0279-Perfect_Squares/main-2.cpp
+++ 0279-Perfect_Squares/main-2.cpp
@@ -1,72 +1,40 @@
-#include<iostream>
-#include<vector>
-#include<climits>
-using namespace std;
-
 class Solution {
-private:
-    bool g_debug = true;
-    vector<int> memo;
-    int calcSquare(int n) {
-        if (g_debug)
-            cout << "[ " << n << " ] enter" << endl;
-        if (n <= 0) return 0;
-        //if (n == 1) return 1;
-        if (memo[n] != -1) return memo[n];
-        int res = INT_MAX;
-        int i = 1;
-        for (; i * i < n; i++) {
-            if (g_debug)
-                cout << "[ " << n << " ] : " << i * i << " + [ " << n - i * i << " ]" << endl;
-            res = min(res, calcSquare(n - i * i) + 1);
-        }
-        // 这里不要漏掉 n 本身就是完全平方数的情况，此时返回的就是1
-        if (i * i == n)
-            res = 1;
-        if (g_debug)
-            cout << "[ " << n << " ] return " << res << endl;
-        memo[n] = res;
-        return res;
-    }
 public:
     int numSquares(int n) {
-        memo = vector<int>(n + 1, -1);
-        return calcSquare(n);
+
+        // 用pair结构同时记录顶点，与此点距源点的距离（即广度优先遍历的层数）
+        queue<pair<int, int>> q;
+        q.push(make_pair(n, 0));
+
+        // 为每个点做标记，确保每个点只进入queue一次
+        // 避免图上同一个顶点，做为相邻顶点多次被推入队列，造成大量重复计算
+        //vector<bool> visited = vector<bool>(n+1, false);
+        vector<bool> visited(n + 1, false);
+        visited[n] = true;
+
+        // dfs
+        while (!q.empty()) {
+            int num = q.front().first;
+            int level = q.front().second;
+            q.pop();
+            // 每次出队的点，把没入过队的相邻顶点都推入队列
+            // 这些由出队顶点推入的点，是此出队顶点下一层上面的点
+            for (int i = 0; i * i <= num; i++) {
+                int tmp = num - i*i;
+                if (!visited[tmp]) {
+                    // 当找到目标点时，直接返回目标点的层数即可
+                    if (tmp == 0)
+                        return level + 1;
+                    visited[tmp] = true;
+                    q.push(make_pair(tmp, level + 1));
+                }
+            }
+        }
+
+        // error:control reaches end of non-void function[-Werror=return-type]
+        // 在函数的最后一定要返回对应的数据类型，哪怕是永远也用不上
+        // return -1;
+        // 但因为返回的是无意义的值，可以写成抛出一个异常来代替dummy value
+        throw invalid_argument("No Solution.");
     }
 };
-
-//class Solution {
-//private:
-    //bool g_debug = true;
-    //vector<int> memo;
-    //int calcSquare(int n) {
-        //if (g_debug)
-            //cout << "[ " << n << " ] enter" << endl;
-        //if (n <= 0) return 0;
-        //if (n == 1) return 1;
-        //if (memo[n] != -1) return memo[n];
-        //int res = INT_MAX;
-        //int i = 1;
-        //for (; i * i < n; i++) {
-        //if (g_debug)
-            //cout << "[ " << n << " ] : " << i * i << " + [ " << n - i * i << " ]" << endl;
-            //res = min(res, calcSquare(n - i * i) + 1);
-        //if (g_debug)
-            //cout << "[ " << n << " ] return " << res << endl;
-        //}
-        //if (i * i == n)
-            //res = 1;
-        //memo[n] = res;
-        //return res;
-    //}
-//public:
-    //int numSquares(int n) {
-        //memo = vector<int>(n + 1, -1);
-        //return calcSquare(n);
-    //}
-//};
-
-int main() {
-    cout << Solution().numSquares(12) << endl;
-    return 0;
-}
