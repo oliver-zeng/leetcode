ef89ac6bd47704ab32024bb3e30d0a5007e46d1e
oliver-zeng <964994927@qq.com>
2020-03-01 21:49:04 +0800

    update 0104

---
+++ 0104-Maximum_Depth_of_Binary_Tree/main-2.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+// 递推式解法，原问题答案是通过子问题得到的
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root)
+            return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
